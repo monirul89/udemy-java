841904a8fefddc2bd68505de51ed521a2b31c7c2
MD. MONIRUL ISLAM <monirul89@gmail.com>
2018-11-03 19:04:54 +0600

    Create JavaScript-Basic.js

---
+++ JavaScript-Basic.js
@@ -0,0 +1,3 @@
+What is javaScript?
+ object-oriented computer programming language commonly used to create interactive effects within web browsers.
+ and javaScript is a lightweight, cross-platform.
